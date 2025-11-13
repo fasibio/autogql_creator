fd8218831a8e9e10c26f1ce3b74008092fb473f7
Fabian Simon <fabian.simon@inovex.de>
2025-11-13 07:44:43 +0100

    added tests and github action

---
+++ integration_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+	"github.com/machinebox/graphql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const addUserMut = `
+mutation addUser {                                                                           
+		addUser(                                                                                   
+			input: { id: 1, name: "Did you see the autoincrement feature?", todos: [] }              
+		) {                                                                                        
+			affected {                                                                               
+				id                                                                                     
+				name                                                                                   
+			}                                                                                        
+		}                                                                                          
+	}
+`
+
+const addTodoMut = `
+mutation addTodo {                                                                           
+	addTodo(input: { id: 1, text: "Start writing autogql", userID: 1, done: false }) {         
+		affected {                                                                               
+			id                                                                                     
+			text                                                                                   
+			done                                                                                   
+		}                                                                                        
+	}                                                                                          
+}
+`
+
+const QueryTodos = `
+	query todos {                                                                                
+		queryTodo(filter: { user: { name: { startsWith: "Did" } } }) {                             
+			data {                                                                                   
+				id                                                                                     
+				text                                                                                   
+				user {                                                                                 
+					name                                                                                 
+				}                                                                                      
+			}                                                                                        
+		}                                                                                          
+}   `
+
+func TestCreation(t *testing.T) {
+	testFolder := "integration_test_output"
+	defer func() {
+		os.RemoveAll(testFolder) //nolint:errcheck //there is nothing we can do here
+	}()
+	assert := assert.New(t)
+	require := require.New(t)
+	ctx := context.Background()
+
+	r := Runner{
+		Cfg: &Config{
+			Path: testFolder,
+		},
+	}
+
+	err := r.Create(ctx, nil)
+	assert.NoError(err)
+
+	runCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	go func() {
+		r.ExecuteAtFolder(runCtx, "go", "run", "server.go") //nolint:errcheck //there is nothing we can do here
+	}()
+
+	serverUrl := "http://localhost:8080"
+	playgroundUrl, err := url.JoinPath(serverUrl, "playground")
+	require.NoError(err)
+	queryUrl, err := url.JoinPath(serverUrl, "query")
+	require.NoError(err)
+	require.Eventually(func() bool {
+
+		res, err := http.Get(playgroundUrl)
+		if err != nil {
+			return false
+		}
+		if res.StatusCode != 200 {
+			return false
+		}
+		return true
+	}, 20*time.Second, 1*time.Second)
+
+	client := graphql.NewClient(queryUrl)
+	for _, r := range []string{addUserMut, addTodoMut, QueryTodos} {
+		req := graphql.NewRequest(r)
+		var res any
+		require.NoError(client.Run(ctx, req, &res))
+		snaps.MatchJSON(t, res)
+
+	}
+}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"syscall"
 
 	"github.com/urfave/cli/v3"
 
@@ -101,16 +102,26 @@ func (r *Runner) checkPathIsDirEmptyOrNotExist() error {
 	return nil
 }
 
-func (r *Runner) ExecuteAtFolder(name string, arg ...string) error {
-	return r.ExecuteAtFolderWithMap(true, name, arg...)
+func (r *Runner) ExecuteAtFolder(ctx context.Context, name string, arg ...string) error {
+	return r.ExecuteAtFolderWithMap(ctx, true, name, arg...)
 }
 
-func (r *Runner) ExecuteAtFolderWithMap(mapPrints bool, name string, arg ...string) error {
-	cmd := exec.Command(name, arg...)
+func (r *Runner) ExecuteAtFolderWithMap(ctx context.Context, mapPrints bool, name string, arg ...string) error {
+	cmd := exec.CommandContext(ctx, name, arg...)
+	go func() {
+		<-ctx.Done()
+		// Kill the whole process group
+		if cmd.Process != nil {
+			syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL) //nolint:errcheck //there is nothing we can do when an error is happen here
+		}
+	}()
 	cmd.Dir = string(r.Cfg.Path)
 	if mapPrints {
 		cmd.Stderr = os.Stderr
 		cmd.Stdout = os.Stdout
+	}
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true, // create new process group
 	}
 	err := cmd.Run()
 	if err != nil {
@@ -119,39 +130,39 @@ func (r *Runner) ExecuteAtFolderWithMap(mapPrints bool, name string, arg ...stri
 	return nil
 }
 
-func (r *Runner) ExecuteTidy() error {
-	return r.ExecuteAtFolder("go", "mod", "tidy")
+func (r *Runner) ExecuteTidy(ctx context.Context) error {
+	return r.ExecuteAtFolder(ctx, "go", "mod", "tidy")
 }
 
 func (r *Runner) Create(ctx context.Context, c *cli.Command) error {
 	if err := r.checkPathIsDirEmptyOrNotExist(); err != nil {
 		return err
 	}
-	if err := r.ExecuteAtFolder("go", "mod", "init", r.Cfg.GoPath); err != nil {
+	if err := r.ExecuteAtFolder(ctx, "go", "mod", "init", r.Cfg.GoPath); err != nil {
 		return err
 	}
 	if err := os.WriteFile(path.Join(r.Cfg.Path, "tools.go"), toolsGoFile, 0644); err != nil {
 		return err
 	}
-	if err := r.ExecuteTidy(); err != nil {
+	if err := r.ExecuteTidy(ctx); err != nil {
 		return err
 	}
-	if err := r.ExecuteAtFolder("go", "run", "github.com/99designs/gqlgen", "init"); err != nil {
+	if err := r.ExecuteAtFolder(ctx, "go", "run", "github.com/99designs/gqlgen", "init"); err != nil {
 		return err
 	}
-	if err := r.ExecuteTidy(); err != nil {
-		return err
-	}
-
-	if err := r.ExecuteAtFolder("git", "init"); err != nil {
+	if err := r.ExecuteTidy(ctx); err != nil {
 		return err
 	}
 
-	if err := r.ExecuteAtFolder("git", "add", "."); err != nil {
+	if err := r.ExecuteAtFolder(ctx, "git", "init"); err != nil {
 		return err
 	}
 
-	if err := r.ExecuteAtFolder("git", "commit", "-m", "gqlgen init finished"); err != nil {
+	if err := r.ExecuteAtFolder(ctx, "git", "add", "."); err != nil {
+		return err
+	}
+
+	if err := r.ExecuteAtFolder(ctx, "git", "commit", "-m", "gqlgen init finished"); err != nil {
 		return err
 	}
 
@@ -161,7 +172,7 @@ func (r *Runner) Create(ctx context.Context, c *cli.Command) error {
 	if err := os.WriteFile(path.Join(r.Cfg.Path, "plugin", "main.go"), pluginMainGoFile, 0644); err != nil {
 		return err
 	}
-	if err := r.ExecuteTidy(); err != nil {
+	if err := r.ExecuteTidy(ctx); err != nil {
 		return err
 	}
 	schemaFilePath := path.Join(r.Cfg.Path, "graph", "schema.graphqls")
@@ -181,11 +192,9 @@ func (r *Runner) Create(ctx context.Context, c *cli.Command) error {
 		return err
 	}
 
-	if err := r.ExecuteAtFolderWithMap(false, "go", "run", "plugin/main.go"); err != nil {
-		//no break error is fine
+	r.ExecuteAtFolderWithMap(ctx, false, "go", "run", "plugin/main.go") //nolint:errcheck //we know it will cotains but this is fine at the pipe
 
-	}
-	if err := r.ExecuteTidy(); err != nil {
+	if err := r.ExecuteTidy(ctx); err != nil {
 		return err
 	}
 
@@ -197,7 +206,7 @@ func (r *Runner) Create(ctx context.Context, c *cli.Command) error {
 		return err
 	}
 
-	if err := r.ExecuteAtFolder("gopls", "imports", "-w", path.Join("graph/resolver.go")); err != nil {
+	if err := r.ExecuteAtFolder(ctx, "gopls", "imports", "-w", path.Join("graph/resolver.go")); err != nil {
 		return err
 	}
 
@@ -210,11 +219,11 @@ func (r *Runner) Create(ctx context.Context, c *cli.Command) error {
 		return err
 	}
 
-	if err := r.ExecuteTidy(); err != nil {
+	if err := r.ExecuteTidy(ctx); err != nil {
 		return err
 	}
 
-	if err := r.ExecuteAtFolder("gopls", "imports", "-w", path.Join("server.go")); err != nil {
+	if err := r.ExecuteAtFolder(ctx, "gopls", "imports", "-w", path.Join("server.go")); err != nil {
 		return err
 	}
 	fmt.Print(docFile)
